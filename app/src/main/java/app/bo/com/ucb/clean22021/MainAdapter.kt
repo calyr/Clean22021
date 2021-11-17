87609db597544378bb1c63abf9879120319cdf19
Roberto Carlos Callisaya Mamani <calyr.software@gmail.com>
2021-11-16 20:11:58 -0400

    added Picasso

--- app/src/main/java/app/bo/com/ucb/clean22021/MainAdapter.kt
+++ app/src/main/java/app/bo/com/ucb/clean22021/MainAdapter.kt
@@ -1,12 +1,15 @@
 package app.bo.com.ucb.clean22021
 
 import android.content.Context
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import app.bo.com.ucb.domain.Movie
+import com.squareup.picasso.Picasso
 
 class MainAdapter(val list: List<Movie>, val context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -19,6 +22,8 @@ class MainAdapter(val list: List<Movie>, val context: Context) : RecyclerView.Ad
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val movie = list.get(position)
         holder.itemView.findViewById<TextView>(R.id.tv_movie_title).text = movie.title
+        Picasso.get().load("https://image.tmdb.org/t/p/w185/${movie.posterPath}")
+            .into(holder.itemView.findViewById<ImageView>(R.id.img_movie))
     }
 
     override fun getItemCount(): Int {
